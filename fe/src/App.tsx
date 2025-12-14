f0d81a7cf09f010e5ec0fbcb4aa0cef873948f67
nam <22130173@st.hcmuaf.edu.vn>
2025-12-14 19:56:33 +0700

    chore: update styles to use SCSS and add sass-embedded dependency

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -3,7 +3,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Nav from './components/NavigationBar/Nav';
 import Home from './pages/Home/Home';
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
