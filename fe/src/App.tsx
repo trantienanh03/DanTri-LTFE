22bdd855ebb880a01edaca4b26e7a445d3bf7618
Hai-Hay-Ho <hominhhai2k4@gmail.com>
2025-12-17 20:34:03 +0700

    chuyển trang login thành tab cho giống trang gốc

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -4,41 +4,45 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Nav from './components/NavigationBar/Nav';
 import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
 import ArticleDetail from './pages/ArticleDetail/ArticleDetail';
+import AuthModal from './components/AuthModal/AuthModal';
+import { AuthProvider } from './context/AuthContext';
 import './App.scss'
 
 function App() {
   return (
     <div className="app">
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        
-        {/* Route cho trang chi tiết bài viết */}
-        <Route path="/article/:id" element={
-          <>
-            <Banner />
-            <Header />
-            <Nav />
-            <main className="body-container">
-              <ArticleDetail />
-            </main>
-            <Footer />
-          </>
-        } />
-        
-        <Route path="/" element={
-          <>
-            <Banner />
-            <Header />
-            <Nav />
-            <main className="body-container">
-              <Home />
-            </main>
-            <Footer />
-          </>
-        } />
-      </Routes>
+      <AuthProvider>
+        <AuthModal />
+        <Routes>
+
+          
+          {/* Route cho trang chi tiết bài viết */}
+          <Route path="/article/:id" element={
+            <>
+              <Banner />
+              <Header />
+              <Nav />
+              <main className="body-container">
+                <ArticleDetail />
+              </main>
+              <Footer />
+            </>
+          } />
+          
+          <Route path="/" element={
+            <>
+              <Banner />
+              <Header />
+              <Nav />
+              <main className="body-container">
+                <Home />
+              </main>
+              <Footer />
+            </>
+          } />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
